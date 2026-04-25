59e5370f8667d79cd7361dd2ad8366e453488565
Fábio Parente Martins Santos <fabiomartinssantos@MacBookPro.home>
2026-04-24 21:15:57 -0300

    config: aumenta limite de upload para 50mb

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "50mb",
     },
   },
+  api: {
+    bodyParser: {
+      sizeLimit: "50mb",
+    },
+    responseLimit: "50mb",
+  },
 };
-
 export default nextConfig;
