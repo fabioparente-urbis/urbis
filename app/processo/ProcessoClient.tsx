1b4cba53a3b68cb66e0cc6355e6a151c80ed994c
Fábio Parente <fabio.parente@gmail.com>
2026-05-14 09:50:21 -0300

    fix: substituir ** por Math.pow na conversão UTM

--- app/processo/ProcessoClient.tsx
+++ app/processo/ProcessoClient.tsx
@@ -112,19 +112,19 @@ function utmToLatLng(easting: number, northing: number): { lat: number; lng: num
   const y = northing - 10000000;
   const lon0 = (22 - 1) * 6 - 180 + 3;
   const M = y / k0;
-  const mu = M / (a * (1 - e**2/4 - 3*e**4/64 - 5*e**6/256));
+  const mu = M / (a * (1 - Math.pow(e,2)/4 - 3*Math.pow(e,4)/64 - 5*Math.pow(e,6)/256));
   const e1 = (1 - Math.sqrt(1 - e*e)) / (1 + Math.sqrt(1 - e*e));
-  const fp = mu + (3*e1/2 - 27*e1**3/32)*Math.sin(2*mu)
-           + (21*e1**2/16 - 55*e1**4/32)*Math.sin(4*mu)
-           + (151*e1**3/96)*Math.sin(6*mu)
-           + (1097*e1**4/512)*Math.sin(8*mu);
-  const C1 = e1sq * Math.cos(fp)**2;
-  const T1 = Math.tan(fp)**2;
-  const R1 = a*(1-e*e) / (1-(e*Math.sin(fp))**2)**1.5;
-  const N1 = a / Math.sqrt(1-(e*Math.sin(fp))**2);
+  const fp = mu + (3*e1/2 - 27*Math.pow(e1,3)/32)*Math.sin(2*mu)
+           + (21*Math.pow(e1,2)/16 - 55*Math.pow(e1,4)/32)*Math.sin(4*mu)
+           + (151*Math.pow(e1,3)/96)*Math.sin(6*mu)
+           + (1097*Math.pow(e1,4)/512)*Math.sin(8*mu);
+  const C1 = e1sq * Math.pow(Math.cos(fp),2);
+  const T1 = Math.pow(Math.tan(fp),2);
+  const R1 = a*(1-e*e) / Math.pow(1-Math.pow(e*Math.sin(fp),2),1.5);
+  const N1 = a / Math.sqrt(1-Math.pow(e*Math.sin(fp),2));
   const D = x / (N1*k0);
-  const lat = fp - (N1*Math.tan(fp)/R1)*(D**2/2 - (5+3*T1+10*C1-4*C1**2-9*e1sq)*D**4/24 + (61+90*T1+298*C1+45*T1**2-252*e1sq-3*C1**2)*D**6/720);
-  const lng = lon0*Math.PI/180 + (D - (1+2*T1+C1)*D**3/6 + (5-2*C1+28*T1-3*C1**2+8*e1sq+24*T1**2)*D**5/120)/Math.cos(fp);
+  const lat = fp - (N1*Math.tan(fp)/R1)*(Math.pow(D,2)/2 - (5+3*T1+10*C1-4*Math.pow(C1,2)-9*e1sq)*Math.pow(D,4)/24 + (61+90*T1+298*C1+45*Math.pow(T1,2)-252*e1sq-3*Math.pow(C1,2))*Math.pow(D,6)/720);
+  const lng = lon0*Math.PI/180 + (D - (1+2*T1+C1)*Math.pow(D,3)/6 + (5-2*C1+28*T1-3*Math.pow(C1,2)+8*e1sq+24*Math.pow(T1,2))*Math.pow(D,5)/120)/Math.cos(fp);
   return { lat: lat*180/Math.PI, lng: lng*180/Math.PI };
 }
 
